3097fdb74d0fdc0c7840ff555a6ce4caba47be89
Hamed Hematian <70624162+HamedHematian@users.noreply.github.com>
2025-05-29 09:49:34 -0500

    Update llava-pope.py

--- LLaVA/llava-pope.py
+++ LLaVA/llava-pope.py
@@ -4,7 +4,7 @@ from llava.model.builder import load_pretrained_model
 from llava.mm_utils import get_model_name_from_path
 from llava.eval.run_llava import eval_model
 import os
-model_path = "../model_weights/llava-v1.5-7b"
+model_path = "liuhaotian/llava-v1.5-7b"
 
 tokenizer, model, image_processor, context_len = load_pretrained_model(
     model_path=model_path,
